612d5fa8841199dc8f37b88c867205aba6ba6da5
Ravi Bechoe <ravibechoe@outlook.com>
2023-02-05 10:33:58 +0100

    added fast ending

---
+++ Roots/Assets/Scripts/Other/fast ending.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class fastending : MonoBehaviour
+{
+    bool selected = false;
+    public GameObject ui;
+    public GameObject audioEnd;
+    public GameObject wallEnd;
+    public GameObject[] disables;
+
+    Materialoffset mos;
+
+    void Start()
+    {
+        mos = wallEnd.GetComponent<Materialoffset>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (selected)
+        {
+            ui.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                audioEnd.GetComponent<AudioSource>().Play();
+                StartCoroutine(StartEnding());
+
+                foreach (GameObject obj in disables)
+                {
+                    obj.SetActive(false);
+                }
+            }
+        }
+        else
+        {
+            ui.SetActive(false);
+        }
+    }
+
+    IEnumerator StartEnding()
+    {
+        for (int i = 0; i < 20; i++)
+        {
+            mos.scrollspeed += 0.025f;
+            yield return new WaitForSeconds(1);
+        }
+        yield return new WaitForSeconds(2);
+
+        SceneManager.LoadScene(0);
+    }
+
+    private void OnMouseEnter()
+    {
+        selected = true;
+    }
+
+    private void OnMouseExit()
+    {
+        selected = false;
+    }
+}
