4adb0e47032083e7dbd1dc78b46b8d7e49da5bc8
Ravi Bechoe <ravibechoe@outlook.com>
2023-02-05 01:29:50 +0100

    Added PP

---
+++ Roots/Assets/Scripts/Other/Wavey.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wavey : MonoBehaviour
+{
+    public float distance = 5f;
+    public float speed = 1f;
+    public float timeOffset = 0; // makes it a bit random
+
+    public bool axisX, axisY, axisZ;
+
+    private Vector3 startEuler;
+
+    public bool noRandom;
+
+    private void Start()
+    {
+        startEuler = transform.localEulerAngles;
+        if (!noRandom) timeOffset = Random.Range(0, 1000);
+    }
+
+    private void Update()
+    {
+        float val = Mathf.Sin(Time.time * speed + timeOffset) * distance;
+        float valX = (axisX) ? val : 0;
+        float valY = (axisY) ? val : 0;
+        float valZ = (axisZ) ? val : 0;
+        transform.localEulerAngles = startEuler + new Vector3(valX, valY, valZ);
+    }
+}
---
+++ Roots/Assets/Scripts/Player/VFXTuner.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.HighDefinition;
+
+public class VFXTuner : MonoBehaviour
+{
+    VolumeManager volumeManager;
+
+    void Start()
+    {
+        // TODO how to get bloom :(
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
